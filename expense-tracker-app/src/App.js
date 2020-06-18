bf0e7e52434d212e098daad09aaa22900195ff96
umairsadiq88 <umairsadiq2010@gmail.com>
2020-06-18 17:00:55 +0500

    update app

--- expense-tracker-app/src/App.js
+++ expense-tracker-app/src/App.js
@@ -1,22 +1,29 @@
 import React from "react";
+
+// Import CSS
 import "./App.css";
 
+// Import Components
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
-import { Accountsummary } from "./components/Accountsummary";
+import { AccountSummary } from "./components/AccountSummary";
 import { TransactionHistory } from "./components/TransactionHistory";
+import { AddTransaction } from "./components/AddTransaction";
+
+// Import Provider
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
-
       <div className="container">
         <Balance />
-        <Accountsummary />
+        <AccountSummary />
         <TransactionHistory />
+        <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
--- expense-tracker-app/src/components/TransactionHistory.js
+++ expense-tracker-app/src/components/TransactionHistory.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, {useContext} from "react";
+
+//Import Transaction Component
+import { Transaction } from './Transaction';
+
+//Import the Global State
+
+import { globalContext} from '../context/GlobalState';
 
 export const TransactionHistory = () => {
+
+  const { Transaction } = useContext(GlobalContext);
+
   return (
     <div>
-      <h3>Transaction History</h3>
-      <ul className="list">
-        <li className="plus">
-          Project 1 Income <span>$1,000</span>
-          <button className="delete-btn"> X </button>
-        </li>
-        <li className="minus">
-          Project 1 Salaries <span>-$500</span>
-          <button className="delete-btn"> X </button>
-        </li>
-      </ul>
+        <h3>
+            Transaction History
+        </h3>
+        <ul className="list">
+            {transactions.map(transaction => 
+                (
+                <Transaction key={transaction.id} transaction={transaction} />
+                )
+            )}
+        </ul>
     </div>
-  );
-};
+)
+}
